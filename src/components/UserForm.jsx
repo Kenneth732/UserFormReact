c529ce973ce697738f8642f9f00fd916e80ef45e
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-14 09:16:25 +0300

    first commit

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function UserForm(){
+    return(
+        <div></div>
+    )
+}
+export default UserForm;
---
+++ src/components/UserPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function UserPage(){
+    return(
+        <div></div>
+    )
+}
+export default UserPage;
