6ac8181ce1f83b0ef84f9ecaccb549a1aebc6ab7
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-14 09:26:00 +0300

    useEff.. to fetch data from db.json

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function UserPage(){
+    const [userData, setUserData] = useState([])
+
+    useEffect(() => {
+        fetch('')
+        .then((res) => res.json())
+        .then((userArrays) => {
+            
+        })
+    })
     return(
         <div></div>
     )
